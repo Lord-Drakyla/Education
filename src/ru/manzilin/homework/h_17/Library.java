eec3482c6139ad0e6cd9abdc27ba43d161ae1fb8
Lord-Drakyla <DrakylaV@gmail.com>
2018-09-22 09:49:27 +0300

    д\з № 17 Начато

--- src/ru/manzilin/homework/h_17/Library.java
+++ src/ru/manzilin/homework/h_17/Library.java
@@ -6,10 +6,14 @@ public class Library implements Serializable {
     private static final long serialVersionUID = -2678311517410866040L;
 
 
+
     // TODO добавлять книгу в библиотеку.
 
+
     // TODO показывать список книг в библиотеке.
+
     // TODO восстанавливать содержимое библиотеки после перезапуска.
+
     // TODO показывать соответствующее сообщение в случае ошибок.
 
 }
