d5aa6377fe2a724cb28b627994f623159c47ede6
Lord-Drakyla <Drakylav@gmail.com>
2018-09-23 13:25:38 +0300

    Д\З 8
    Создан класс Акта

---
+++ src/ru/manzilin/homework/h_08/Task5/Act.java
@@ -0,0 +1,19 @@
+package ru.manzilin.homework.h_08.Task5
+
+public class Act {
+
+  private int number;
+	private String[] listOfGoods;
+	private LocalDate date;
+	
+  Act(){
+  
+	}
+  Act(int number, String[] listOfGoods, LocalDate date){
+  this.number = number;
+  this.listOfGoods=listOfGoods;
+  this.date=date;
+	}
+	
+	
+}
