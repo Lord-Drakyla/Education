1c88b29b987a03d7e465c23e819cba1211d35928
Lord-Drakyla <Drakylav@gmail.com>
2018-09-23 14:45:45 +0300

    Д/З 8
    Класс для конвертирования

---
+++ src/ru/manzilin/homework/h_08/Task5/Converter.java
@@ -0,0 +1,11 @@
+package ru.manzilin.homework.h_08.Task5
+
+public class Converter {
+
+  public static Act convertContractToAct(Contract contract){
+  return new Act (contact.getNumber(), contract.getList(), contract.getDate())
+  }
+  
+  
+	
+}
