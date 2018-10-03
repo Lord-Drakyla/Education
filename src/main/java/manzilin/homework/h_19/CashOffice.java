98ae752f02cabd27da0a48d256450592d6ee5f58
Lord-Drakyla <DrakylaV@gmail.com>
2018-10-03 16:47:33 +0300

    Д\з 19 Решение Просто вариант считывания списка продуктов и записи обратно. Чисто императивно.

--- src/main/java/manzilin/homework/h_19/CashOffice.java
+++ src/main/java/manzilin/homework/h_19/CashOffice.java
@@ -1,5 +1,5 @@
 package manzilin.homework.h_19;
-
+//TODO попрообовать сделать более разносторонним.
 public class CashOffice {
     public static void main(String[] args) {
         CashBill cashBill = new CashBill();
