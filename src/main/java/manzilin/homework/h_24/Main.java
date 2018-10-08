2c163992961f8065fff5ce0f3f0f2c93f0a503e9
Lord-Drakyla <DrakylaV@gmail.com>
2018-10-08 11:47:58 +0300

    Д\з 24 Исключена ошибка при обращении к null для коллекции

--- src/main/java/manzilin/homework/h_24/Main.java
+++ src/main/java/manzilin/homework/h_24/Main.java
@@ -7,15 +7,14 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Main {
-    private static String[] test = new String[]{"foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude",null};
+    private static String[] test = new String[]{"foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude", null};
 
     /**
      * Можно было добавить иным образом
      * for (String s : test
-     *         ) {
-     *             if (s != null) set.add(s);
-     *         }
-     *
+     * ) {
+     * if (s != null) set.add(s);
+     * }
      */
     public static void main(String[] args) {
         Set<String> set = new LinkedHashSet<>(Arrays.asList(test));
@@ -33,6 +32,7 @@ public class Main {
      */
     public static Set<String> removeEvenLength(@NotNull Set<String> set) {
         Set<String> newSet = new LinkedHashSet<>();
+        set.remove(null);
         newSet.addAll(set);
         for (String s : set) {
             if (s.length() % 2 == 0) newSet.remove(s);
