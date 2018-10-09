77048c67f02d74644ed50573b3c90590d612a6e5
Lord-Drakyla <DrakylaV@gmail.com>
2018-10-09 22:37:24 +0300

    Д\з 25 Решение в лоб можно уменьшить на строку.

--- src/main/java/manzilin/homework/h_25/Task1/Main.java
+++ src/main/java/manzilin/homework/h_25/Task1/Main.java
@@ -12,28 +12,34 @@ public class Main {
         mymap.put("Виктор", "Сидоров");
         mymap.put("Сергей", "Савельев");
         mymap.put("Вадим", "Викторов");
-        System.out.println("Для первого случая "+isUniqueSimple(mymap));
+        System.out.println("Для первого случая " + isUniqueSimple(mymap));
 
         mymap.put("Вася", "Иванов");
         mymap.put("Петр", "Петров");
         mymap.put("Виктор", "Иванов");
         mymap.put("Сергей", "Савельев");
         mymap.put("Вадим", "Петров");
-        System.out.println("Для второго случая "+isUniqueSimple(mymap));
+        System.out.println("Для второго случая " + isUniqueSimple(mymap));
 
     }
 
     /**
+     *
+     *
+     * Варианты заполнения [набора] из [карты]
+     * Set<String> set = new HashSet<>()
      * for (String s:map.values()) {
      * set.add(s);
      * }
+     *
+     * Set<String> set = new HashSet<>()
+     * set.addAll(map.values());
      *
      * @param map
      * @return
      */
     static public boolean isUniqueSimple(Map<String, String> map) {
-        Set<String> set = new HashSet<>();
-        set.addAll(map.values());
-        return set.size()==map.values().size();
+        Set<String> set = new HashSet<>(map.values());
+        return set.size() == map.values().size();
     }
 }
