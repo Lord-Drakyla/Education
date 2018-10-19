09543ecc0326b061977ac22a5c562f581b21e237
Lord-Drakyla <DrakylaV@gmail.com>
2018-10-19 05:55:36 +0300

    Д\з 24 рефакторинг тестов

--- src/test/java/manzilin/homework/h_24/MainTest.java
+++ src/test/java/manzilin/homework/h_24/MainTest.java
@@ -9,7 +9,8 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    private static String[] source= new String[]{"foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude", null};
+    private static String[] source= new String[]
+            {"foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude", null};
     private static String[] expected = new String[]{"foo", "bar", "spoon", "!"};
 
     @Test
@@ -17,7 +18,8 @@ class MainTest {
         Set<String> sourceSet = new LinkedHashSet<>(Arrays.asList(source));
         Set<String> expectedSet = new LinkedHashSet<>(Arrays.asList(expected));
         sourceSet = SetToNoEven.removeEvenLength(sourceSet);
-        assertEquals(expectedSet,sourceSet, "Должны остаться только слова с нечетным количеством символов");
+        assertEquals(expectedSet,sourceSet,
+                "Должны остаться только слова с нечетным количеством символов");
     }
 
     @Test
@@ -25,6 +27,7 @@ class MainTest {
         Set<String> sourceSet = new LinkedHashSet<>(Arrays.asList(source));
         Set<String> expectedSet = new LinkedHashSet<>(Arrays.asList(expected));
         sourceSet = SetToNoEven.removeEvenLengthAnother(sourceSet);
-        assertEquals(expectedSet,sourceSet, "Должны остаться только слова с нечетным количеством символов");
+        assertEquals(expectedSet,sourceSet,
+                "Должны остаться только слова с нечетным количеством символов");
     }
 }
