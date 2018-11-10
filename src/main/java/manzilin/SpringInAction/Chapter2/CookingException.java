1cfc894425c21e057475f8ff2a700d3bd14514af
Lord-Drakyla <DrakylaV@gmail.com>
2018-11-10 09:21:32 +0300

    Спринг в действии. Добавление конфигурационного файла и запускаюшего класса

---
+++ src/main/java/manzilin/SpringInAction/Chapter2/CookingException.java
@@ -0,0 +1,7 @@
+package manzilin.SpringInAction.Chapter2;
+
+public class CookingException extends RuntimeException {
+    public CookingException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/manzilin/SpringInAction/Chapter2/Starter.java
@@ -0,0 +1,17 @@
+package manzilin.SpringInAction.Chapter2;
+
+import manzilin.SpringInAction.Chapter2.Cooks.Cook;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Starter {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "springKitchen.xml");
+        Cook cook = (Cook) ctx.getBean("Ivanov");
+        cook.Dish();
+        System.out.println("-----------------------");
+        Cook chefcook = (Cook) ctx.getBean("Shef");
+        chefcook.Dish();
+    }
+}
