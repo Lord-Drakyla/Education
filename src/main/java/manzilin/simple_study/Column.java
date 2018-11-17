697801da419e4e1300d2ffb48841edc024b9e7b1
Lord-Drakyla <DrakylaV@gmail.com>
2018-11-17 07:49:56 +0300

    Уборка Ненужный проект перенесен в качестве доп_информации по работе с базами данных в проект с института. Там нужно организовать работу с компонентами веществ.

--- src/main/java/manzilin/simple_study/Column.java
+++ src/main/java/manzilin/simple_study/Column.java
@@ -313,3 +313,143 @@ public class Column {
 //        return Optimum
 //        end
 //        --
+
+//
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.jdbc.core.*;
+//import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+//
+//import javax.sql.DataSource;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("");
+//        DataSource dataSource = context.getBean(DataSource.class);
+//        JdbcTemplate template = new JdbcTemplate(dataSource);
+//        template.execute("CREATE TABLE Components(" +
+//                "id INTEGER, name VARCHAR(100), CriticalVolume DOUBLE, MolecularWeight DOUBLE, " +
+//                "LIQDEN DOUBLE,  BoilerTemperature DOUBLE, CriticalTemperature DOUBLE," +
+//                "Formula VARCHAR(100), TDEN DOUBLE, CriticalPressure DOUBLE, DIPM DOUBLE," +
+//                "CriticalZ DOUBLE, OMEGA DOUBLE, NormalMeltingPoint DOUBLE)");
+//        template.execute((ConnectionCallback<Object>) con -> {
+//            System.out.println(con.getMetaData().getURL());
+//            System.out.println(con.getMetaData().getUserName());
+//            return null;
+//        });
+//        template.execute("INSERT INTO Components VALUES (" +
+//                "0, 'АРГОН', 74.9000, 39.9480, 1.3730, 87.3000, 150.8000, 'AR', 90.0000," +
+//                "48.1000, 0.0000, 0.2910, -0.0040, 83.8000 )");
+//        Boolean ok = template.execute((StatementCallback<Boolean>) stmt -> {
+//            stmt.execute("INSERT INTO Components VALUES (" +
+//                    "1, 'БОРА ТРИХЛОРИД', 0.0000 ,117.1690 ,1.3500 ,285.7000 ,452.0000, 'BCL3',284.0000," +
+//                    "38.2000, 0.0000, 0.0000, 0.1500, 165.9000 )");
+//            return Boolean.TRUE;
+//        });
+////            template.execute("INSERT INTO Components VALUES (" +
+////                    ", '', , , , ,, '',," +
+////                    ", , , ,  )");
+//
+//        template.execute((PreparedStatementCreator) con -> con.prepareStatement("INSERT INTO Components VALUES (" +
+//                        "?,?,?,?,?,?,?,?,?,?,?,?,?,?)"),
+//                (PreparedStatementCallback<Object>) ps -> {
+//                    ps.setInt(1, 2);
+//                    ps.setString(2, "БОРА ТРИФТОРИД");
+//                    ps.setDouble(3, 0.0000);
+//                    ps.setDouble(4, 67.8050);
+//                    ps.setDouble(5, 0.0000);
+//                    ps.setDouble(6, 173.3000);
+//                    ps.setDouble(7, 260.8000);
+//                    ps.setString(8, "BF3");
+//                    ps.setDouble(9, 0.0000);
+//                    ps.setDouble(10, 49.2000);
+//                    ps.setDouble(11, 0.0000);
+//                    ps.setDouble(12, 0.0000);
+//                    ps.setDouble(13, 0.4200);
+//                    ps.setDouble(14, 146.5000);
+//                    ps.execute();
+//                    return null;
+//                });
+//        NamedParameterJdbcTemplate namedTemplate = new NamedParameterJdbcTemplate(template);
+//        Map<String, Object> params = new HashMap<>();
+//        params.put("id", 3);
+//        params.put("name", "БРОМ");
+//        params.put("CriticalVolume", 127.0000);
+//        params.put("MolecularWeight", 159.8080);
+//        params.put("LIQDEN", 3.1190);
+//        params.put("BoilerTemperature", 331.9000);
+//        params.put("CriticalTemperature", 584.0000);
+//        params.put("Formula", "BR2");
+//        params.put("TDEN", 293.0000);
+//        params.put("CriticalPressure", 102.0000);
+//        params.put("DIPM", 0.2000);
+//        params.put("CriticalZ", 0.2700);
+//        params.put("OMEGA", 0.1320);
+//        params.put("NormalMeltingPoint", 266.0000);
+//        namedTemplate.update("INSERT INTO Components VALUES (:id, :name, :CriticalVolume, :MolecularWeight, :LIQDEN, " +
+//                ":BoilerTemperature, :CriticalTemperature, :Formula, :TDEN, :CriticalPressure,:DIPM, " +
+//                ":CriticalZ, :OMEGA, :NormalMeltingPoint)", params);
+//        System.out.println("ok = " + ok);
+//        template.query("SELECT * FROM Components", (RowCallbackHandler) rs -> {
+//            System.out.println(rs.getString("name"));
+//        });
+//        Integer count = template.queryForObject("SELECT COUNT (*) FROM Components", Integer.class);
+//        System.out.println("count - " + count);
+//        Map<String, Object> customerMap = template.queryForMap("SELECT * FROM Components WHERE id = 1");
+//        System.out.println(customerMap.get("name"));
+//    }
+//}
+
+//package manzilin.KsiTasks.Task1;
+//
+//import java.sql.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:study",
+//                "Rinat", "");
+//             Statement statement = connection.createStatement()
+//        ) {
+//
+//            statement.execute("CREATE TABLE Components(" +
+////            {VC=74.9000,MM=39.9480,LIQDEN=1.3730,TB=87.3000,TC=150.8000,Formula="AR",
+////                    TDEN=90.0000,Number=1.0000,PC=48.1000,DIPM=0.0000,ZC=0.2910,Name="АРГОН",
+////                    OMEGA=-0.0040,TFP=83.8000},
+////            {VC=0.0000,MM=117.1690,LIQDEN=1.3500,TB=285.7000,TC=452.0000,Formula="BCL3",
+////                    TDEN=284.0000,Number=2.0000,PC=38.2000,DIPM=0.0000,ZC=0.0000,Name="БОРА ТРИХЛОРИД",
+////                    OMEGA=0.1500,TFP=165.9000},
+//                    "id INTEGER, name VARCHAR, CriticalVolume DOUBLE, MolecularWeight DOUBLE, " +
+//                    "LIQDEN DOUBLE,  BoilerTemperature DOUBLE, CriticalTemperature DOUBLE," +
+//                    "Formula VARCHAR, TDEN DOUBLE, CriticalPressure DOUBLE, DIPM DOUBLE," +
+//                    "CriticalZ DOUBLE, OMEGA DOUBLE, NormalMeltingPoint DOUBLE)");
+//            statement.execute("INSERT INTO Components VALUES (" +
+//                    "0, 'АРГОН', 74.9000, 39.9480, 1.3730, 87.3000, 150.8000, 'AR', 90.0000," +
+//                    "48.1000, 0.0000, 0.2910, -0.0040, 83.8000 )");
+//            statement.execute("INSERT INTO Components VALUES (" +
+//                    "1, 'БОРА ТРИХЛОРИД', 0.0000 ,117.1690 ,1.3500 ,285.7000 ,452.0000, 'BCL3',284.0000," +
+//                    "38.2000, 0.0000, 0.0000, 0.1500, 165.9000 )");
+////            statement.execute("INSERT INTO Components VALUES (" +
+////                    ", '', , , , ,, '',," +
+////                    ", , , ,  )");
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM Components");
+//            while (resultSet.next()) {
+//                System.out.println(resultSet.getString("name"));
+//            }
+////            Syntax error in SQL statement "CREATE TALE[*]"; expected "OR, FORCE, VIEW,
+////            ALIAS, SEQUENCE, USER, TRIGGER, ROLE, SCHEMA, CONSTANT, DOMAIN, TYPE, DATATYPE,
+////            AGGREGATE, LINKED, MEMORY, CACHED, LOCAL, GLOBAL, TEMP, TEMPORARY, TABLE, SYNONYM,
+////            PRIMARY, UNIQUE, HASH, SPATIAL, INDEX"; SQL statement:
+//
+////            CREATE TALE [42001-197]
+////            statement.execute("CREATE TALE");
+//            resultSet.close();
+////            statement.close();
+////            connection.close();
+//            System.out.println(connection.isClosed());
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//}
